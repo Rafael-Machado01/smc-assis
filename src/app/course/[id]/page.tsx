20a98bddc3cb77519fce943ce340c76aa60b76ac
Rafa <rafael.machado.ampudia01@gmail.com>
2026-07-27 18:37:21 -0300

    final

--- src/app/course/[id]/page.tsx
+++ src/app/course/[id]/page.tsx
@@ -1,21 +1,5 @@
-import CursePage from '@/app/components/CursePage'
-import {coursesData} from "@/app/constants/courses-data";
-import {Metadata} from "next";
-import {useParams} from "next/navigation";
-
-
-const params = useParams()
-const id = Number(params.id)
-
-const nameCourse = coursesData.find(course => course.id === id)
-export const metadata: Metadata = {
-  title: "Home - AssisTáArte",
-  description: "Projeto da Secretaria de Cultura de Assis"
-}
-
+import CoursePage from "@/app/components/CoursePage";
 
 export default function Page() {
-  return(
-    <CursePage/>
-  )
+  return <CoursePage />;
 }
