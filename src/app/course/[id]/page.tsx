bc71e94880bacabe312b576e95ae4606c6011a0a
Rafa <rafael.machado.ampudia01@gmail.com>
2026-07-27 11:41:07 -0300

    benefit component

---
+++ src/app/course/[id]/page.tsx
@@ -0,0 +1,10 @@
+import Benefits from "@/app/components/Benefits";
+
+export default function CoursePage() {
+  const id = 1
+  return(
+    <main className="px-12 mt-20">
+    <Benefits id={id}></Benefits>
+    </main>
+  )
+}
